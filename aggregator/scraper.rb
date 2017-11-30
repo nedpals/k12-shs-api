406deda5851202cf52f1076779c8bbcb06b95676
Ned Palacios <npdoesmc@gmail.com>
2017-11-30 20:03:09 +0800

    Fixed province field

--- aggregator/scraper.rb
+++ aggregator/scraper.rb
@@ -10,7 +10,7 @@ class K12SchoolScraper
       schools "css=table.views-table>tbody>tr.row", :iterator do
         school_id "css=td.views-field-field-school-id"
         region "css=td.views-field-field-region-province"
-        province "css=td.views-field-field-region-province-1"
+        province "css=td.views-field-field-region-province-1>.item-list>ul>.last"
         municipality "css=td.views-field-field-municipality"
         school_name "css=td.views-field-field-school-name"
         programs "css=td.views-field-field-program-offerings" do |p|
@@ -26,11 +26,18 @@ end
 
 scraper = K12SchoolScraper.new
 school_data = {}
-(1..435).each do |page|
-  puts "page: " + page.to_s
-  filename = 'aggregator/data/' + page.to_s + '.json'
-  data = scraper.scan(page)
-  school_data = (page == 1 ? data : school_data.deep_merge(data))
-  # puts school_data
-  File.open(filename, "w+") {|f| f.write(JSON.pretty_generate(data)) }
+
+if File.directory?("aggregator/data")
+  print "Data directory detected"
+  (1..435).each do |page|
+    puts "page: " + page.to_s
+    filename = 'aggregator/data/' + page.to_s + '.json'
+    data = scraper.scan(page)
+    school_data = (page == 1 ? data : school_data.deep_merge(data))
+    # puts school_data
+    File.open(filename, "w+") {|f| f.write(JSON.pretty_generate(data)) }
+  end
+else
+  raise "directory not found. create the folder first then try again."
 end
+
